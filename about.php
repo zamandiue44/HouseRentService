1ee49dfb5f567a08c0ad64a268f85ef735e721d9
SyedZamanMostafiz <zamandiue44@gmail.com>
2017-02-05 02:15:19 +0600

    Project Planning is Starting

---
+++ about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>To-Let</title>
+</head>
+<body>
+	<a href="index.php" target="_blank">Home</a>
+	<a href="about.php" target="_blank">About</a>
+	<h1>Hello Viewer</h1>
+	<?php 
+	echo "<marquee><h1>This is About Page.</h1></marquee>";
+	?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 	<title>To-Let</title>
 </head>
 <body>
-	<p>This is a House Rent Service.</p>
+	<a href="index.php" target="_blank">Home</a>
+	<a href="about.php" target="_blank">About</a>
+	<h1>Hello Viewer</h1>
+	<?php 
+	echo "<marquee>House Rent Service Project Planning is running.</marquee>";
+	?>
 </body>
 </html>
